ba242ad177b9383e2b139ad24b3ed3f4437d57b4
Brian Cavalier <brian@hovercraftstudios.com>
2013-07-16 09:06:35 -0400

    Add brief description to readme, needs examples, API, etc.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Generative Testing
+
+Gent is a generative testing tool for JavaScript.  You describe a scenario, and gent will generate the test inputs and verify the output produced by each using an assertion you provide.
+
--- generator/object.js
+++ generator/object.js
@@ -5,11 +5,10 @@ var next = require('./next');
 var reduce = require('./reduce');
 var take = require('./take');
 
-// object() -> object with random number of keys, each w/random value
-// object(n) -> object with n keys, each w/random value
-// object(n, values) -> object with n keys, each w/value from values generator
-
-// object(keys, values) -> object with keys generator, values generator
+// object() -> object with random number of string() keys, each w/any() value
+// object(n) -> object with n string() keys, each w/any() value
+// object(n, keys) -> object with n keys(), each w/any() value
+// object(n, keys, values) -> object with n keys(), each with values() value
 module.exports = function object(nkeys, keys, values) {
 
 	if(typeof nkeys === 'undefined') {
